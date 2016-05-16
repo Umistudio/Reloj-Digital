4c3cd78ef1d9e2792bfe81294ab15747316208c6
Paty Arakawa <paty.arakawa@gmail.com>
2016-05-15 23:35:05 -0500

    no funciona checked, pero da la hora con segundos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 $(function(){
+
+var mostrarHora=function(){
+//Hora local 
  var fecha = new Date();
 
  var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles',
@@ -7,6 +10,9 @@ $(function(){
  var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
  			 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
+ var segundos=fecha.getSeconds()
+ 	if(segundos<10)
+ 		segundos="0"+segundos
  var minutos=fecha.getMinutes()
  	if(minutos<10)
  		minutos="0"+minutos
@@ -14,8 +20,25 @@ $(function(){
  	if(horas<10)
  		horas="0"+horas
 
- $("#hora").html(horas+":"+minutos)
+ $("#hora").html(horas+":"+minutos+":"+segundos)
  $("#dia").html(semana[fecha.getDay()]+", ")
  $("#fecha").html(fecha.getDate()+" de "+meses[fecha.getMonth()])
   
+ //Hora Regiones
+
+ var regiones=[{region:"México DC",
+				diferencia:0},
+			   {region:"Sao Paulo",
+				diferencia:2},
+			   {region:"Washington DC",
+				diferencia:-1},
+			   {region:"Madrid",
+				diferencia:7}]
+
+	if($("#mexico-dc").checked)
+		$("#otros").html("hola")
+	}
+
+	mostrarHora();
+	var intervalo = setInterval(mostrarHora, 1000);
  })
